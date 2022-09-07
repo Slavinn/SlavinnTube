d0006e31604605c25f0a5dd93e0fd1ab4e60d589
Slavinn <99608449+Slavinn@users.noreply.github.com>
2022-09-07 08:47:33 -0600

    Subscription entity

--- src/main/java/com/example/slavinntube/entity/Subscription.java
+++ src/main/java/com/example/slavinntube/entity/Subscription.java
@@ -1,22 +1,42 @@
 package com.example.slavinntube.entity;
 
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
+
 import java.util.UUID;
 
 @Entity
 @Table(name = "subscription")
 public class Subscription {
+    @Id
+    @Column(name ="subscriberId")
+    private UUID subscriberId;
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    @Column(name = "id", updatable = false, nullable = false)
-    private UUID id;
+    @Column(name ="subscriberToId")
+    private UUID subscriberToId;
+
+    public Subscription() {
+    }
+
+    public Subscription(UUID subscriberId, UUID subscriberToId) {
+        this.subscriberId = subscriberId;
+        this.subscriberToId = subscriberToId;
+    }
 
 
+    public UUID getSubscriberId() {
+        return subscriberId;
+    }
+
+    public void setSubscriberId(UUID subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public UUID getSubscriberToId() {
+        return subscriberToId;
+    }
+
+    public void setSubscriberToId(UUID subscriberToId) {
+        this.subscriberToId = subscriberToId;
+    }
 }
---
+++ src/main/java/com/example/slavinntube/entity/UserList.java
@@ -0,0 +1,2 @@
+package com.example.slavinntube.entity;public class UserList {
+}

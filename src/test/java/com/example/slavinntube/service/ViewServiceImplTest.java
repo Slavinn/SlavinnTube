df52b7e919e63e184b15c0c091ff49c4915766af
Slavinn <99608449+Slavinn@users.noreply.github.com>
2022-09-16 07:46:08 -0600

    view service testing finished

--- src/test/java/com/example/slavinntube/service/ViewServiceImplTest.java
+++ src/test/java/com/example/slavinntube/service/ViewServiceImplTest.java
@@ -13,7 +13,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -37,25 +46,72 @@ class ViewServiceImplTest {
 
     @Test
     void getViewsByVideoId() {
+        UUID viewVideoId = viewOne.getVideoId().getId();
+        given(viewRepository.getByVideoId(viewVideoId)).willReturn((List.of(viewOne)));
+
+        List<View> viewList = viewService.getViewsByVideoId(viewVideoId);
+
+        assertNotNull(viewList);
+        assertThat(viewList.size()).isEqualTo(1);
+
     }
 
     @Test
     void getViewsByUserId() {
+        UUID viewUserId = viewOne.getUserId().getId();
+
+        given(viewRepository.getBYUserId(viewUserId)).willReturn(List.of(viewOne));
+
+        List<View> viewList = viewService.getViewsByUserId(viewUserId);
+
+        assertNotNull(viewList);
+        assertThat(viewList.size()).isEqualTo(1);
     }
 
     @Test
     void getById() {
+        given(viewRepository.getReferenceById(viewOne.getId())).willReturn(viewOne);
+
+        Optional<View> currentView = viewService.getById(viewOne.getId());
+
+        assertNotNull(currentView);
     }
 
     @Test
     void saveView() {
+        given(viewRepository.getReferenceById(viewOne.getId())).willReturn(null);
+
+        given(viewRepository.save(viewOne)).willReturn(viewOne);
+
+        View viewSaved = viewService.saveView(viewOne);
+
+        assertNotNull(viewSaved);
     }
 
     @Test
     void updateView() {
+        given(viewRepository.save(viewOne)).willReturn(viewOne);
+
+        Video newVideo = new Video();
+        viewOne.setVideoId(newVideo);
+
+        User newUser = new User();
+        viewOne.setUserId(newUser);
+
+        View updatedView = viewService.updateView(viewOne);
+
+        assertThat(viewOne.getVideoId()).isEqualTo(newVideo);
+        assertThat(viewOne.getUserId()).isEqualTo(newUser);
     }
 
     @Test
     void deleteById() {
+        UUID viewId = viewOne.getId();
+
+        willDoNothing().given(viewRepository).deleteById(viewId);
+
+        viewService.deleteById(viewId);
+
+        verify(viewRepository, times(1)).deleteById(viewId);
     }
 }
